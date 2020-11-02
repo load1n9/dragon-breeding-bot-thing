481a07853acf1856c8b8be6a4f9572de8422d619
loading <dsrebnik@gmail.com>
2020-11-02 17:26:29 +0000

    hmmm

--- cogs/CreateDragonCommand.py
+++ cogs/CreateDragonCommand.py
@@ -10,6 +10,17 @@ from cogs.utils.db import getUser, createUser
             MythicalDragons[]
           ]"""
 
+"""
+Elements:
+	1: Fire
+	2: Water
+	3: Earth
+	4: Air
+	5: Electric
+	6: Wind
+	7: Metal
+	8: Neon
+"""
 
 class Creation(commands.Cog):
     def __init__(self, client):
@@ -18,7 +29,7 @@ class Creation(commands.Cog):
     @commands.command()
     async def spawn(self, ctx):
         try:
-          print(getUser(ctx.author.id))
+          getUser(ctx.author.id)
         except:
           createUser({
             "data":{
@@ -29,7 +40,7 @@ class Creation(commands.Cog):
         chances = random.randint(1, 1010)
 
         if chances > 0 and chances < 601:
-            CommonEmbed = discord.Embed(title = "Common", colour = discord.Colour.gold())
+            CommonEmbed = discord.Embed(title = "Common")
             await ctx.channel.send(embed=CommonEmbed)
 
         elif chances > 600 and chances < 900:
@@ -37,15 +48,44 @@ class Creation(commands.Cog):
             await ctx.channel.send(embed=RareEmbed)
 
         elif chances > 899 and chances < 1000:
-			EpicEmbed = discord.Embed(title="Epic", colour = discord.Colour.purple())
-			await ctx.channel.send(embed=EpicEmbed)
+          EpicEmbed = discord.Embed(title="Epic", colour = discord.Colour.purple())
+          await ctx.channel.send(embed=EpicEmbed)
 
         elif chances > 999 and chances < 1009:
-            LegendaryEmbed = discord.Embed(title="Legenday", colour = discord.Colour.red())
+            LegendaryEmbed = discord.Embed(title="Legendary", colour = discord.Colour.gold())
             await ctx.channel.send(embed=LegendaryEmbed)
 
         elif chances > 1008 and chances < 1011:
-            await ctx.channel.send("Mythical")
+            MythicalEmbed = discord.Embed(title="Mythical", colour = discord.Colour.blue())
+            await ctx.channel.send(embed=MythicalEmbed)
+            
+    @commands.command()
+    async def shop(self, ctx):
+        shop = discord.Embed(
+            title = "Shop",
+            colour = discord.Colour.gold()
+        )
+        shop.add_field(name="Common elements ($500):", value="""
+                                                    Fire Crate 
+                                                    Water Crate
+                                                    Earth Crate
+                                                    Air Crate
+                                                        """)
+        
+        shop.add_field(name="Special elements ($700):", value="""
+                                                    Electric Crate 
+                                                    Metal Crate
+                                                    Neon Crate
+                                                        """)
+        
+        shop.add_field(name="Basic crates:", value="""
+                                                    Common Crate ($250)
+                                                    Rare Crate ($500)
+                                                    Epic Crate ($1000)
+                                                    Legendary Crate ($2500)
+                                                    Mythical Crate ($10.000)
+                                                        """)
+        await ctx.channel.send(embed=shop)
 
 def setup(client):
     client.add_cog(Creation(client))
